a44db879371186fb439c36569986415ac73b584d
paulbodelot <49484206+paulbodelot@users.noreply.github.com>
2019-04-10 15:06:38 +0200

    Add files via upload

--- Rayon.java
+++ Rayon.java
@@ -10,6 +10,7 @@ public class Rayon
 	public int yFin;
 	public Color couleur;
 	public Equation eqDroite;
+	public ObjetOptique createur;
 	
 	
 	public Rayon(int xD,int yD, int xF, int yF,Color laCouleur) //Crée un rayon entre deux points
@@ -30,6 +31,8 @@ public class Rayon
 		
 		this.eqDroite = new Equation(dirRayon,y0);
 		
+		this.createur = null;
+		
 	}
 	
 	public Rayon(int xD, int yD, int []dir,Color laCouleur) //Crée un rayon entre deux points dont les coordonnées sont rangées dans des tableaux
@@ -44,6 +47,47 @@ public class Rayon
 		int y0 = yDebut-xDebut*dir[1]/dir[0];
 		
 		this.eqDroite = new Equation(dir,y0);
+		
+		this.createur = null;
+		
+	}
+	
+	public Rayon(int xD,int yD, int xF, int yF,Color laCouleur, ObjetOptique createur) //Crée un rayon entre deux points
+	{
+		this.xDebut = xD;
+		this.yDebut = yD;
+		this.xFin = xF;
+		this.yFin = yF;
+		
+		this.couleur = laCouleur;
+		
+		//On crée le vecteur directeur du rayon et on calcule son ordonnée à l'origine pour avoir son équation de droite
+		int[]dirRayon= new int[2];
+		dirRayon[0] = xFin-xDebut;
+		dirRayon[1] = yFin-yDebut;
+		
+		int y0 = yDebut-xDebut*dirRayon[1]/dirRayon[0];
+		
+		this.eqDroite = new Equation(dirRayon,y0);
+		
+		this.createur = createur;
+		
+	}
+	
+	public Rayon(int xD, int yD, int []dir,Color laCouleur, ObjetOptique createur) //Crée un rayon entre deux points dont les coordonnées sont rangées dans des tableaux
+	{
+		this.xDebut = xD;
+		this.yDebut = xD;
+		
+		this.couleur = laCouleur;
+		
+		//On calcule l'ordonnée à l'origine pour avoir son équation de droite
+		
+		int y0 = yDebut-xDebut*dir[1]/dir[0];
+		
+		this.eqDroite = new Equation(dir,y0);
+		
+		this.createur = createur;
 		
 	}
 	
